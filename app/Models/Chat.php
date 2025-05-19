c17acd69bacf0dcead354c0f3abc6cc6fe653489
Md Jahidul Islam <jahidcse181@gmail.com>
2025-05-19 10:28:35 +0600

    Chat and Conversation database tables created with models

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    protected $fillable = ['conversation_id', 'sender_id', 'receiver_id', 'message'];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    protected $fillable = ['user_one_id', 'user_two_id'];
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function userOne()
+    {
+        return $this->belongsTo(User::class, 'user_one_id');
+    }
+
+    public function userTwo()
+    {
+        return $this->belongsTo(User::class, 'user_two_id');
+    }
 }
